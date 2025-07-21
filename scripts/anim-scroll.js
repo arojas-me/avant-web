c72e71089d37c528d1c2b3b162e23a6c203b350b
Alejandro Rojas <alejandrorb9@gmail.com>
2025-07-21 13:23:44 -0400

    subiendo cambios

--- scripts/anim-scroll.js
+++ scripts/anim-scroll.js
@@ -2,14 +2,14 @@ document.addEventListener("DOMContentLoaded", function () {
   const fadeElements = document.querySelectorAll(".fade-in");
 
   const observerOptions = {
-    threshold: 0.2, // Ajusta este valor según cuánto del elemento debe estar visible antes de activar la clase
+    threshold: 0.2,
   };
 
   const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
         entry.target.classList.add("active");
-        observer.unobserve(entry.target); // Remover esta línea si deseas que la animación se active siempre al hacer scroll
+        observer.unobserve(entry.target); 
       }
     });
   }, observerOptions);
--- scripts/reponsive-iframe.js
+++ scripts/reponsive-iframe.js
@@ -4,14 +4,12 @@ document.addEventListener("DOMContentLoaded", function () {
     const heroText = document.getElementById("hero-text");
     const heroViewer = document.getElementById("hero-viewer");
 
-    // Detectar si es móvil
     function isMobile() {
       return window.innerWidth <= 768;
     }
 
-    // Solo mover si estamos en móvil
     if (isMobile() && heroText && heroViewer) {
-      // Mover el viewer justo después del hero-text
+
       heroText.insertAdjacentElement("afterend", heroViewer);
     }
   });
